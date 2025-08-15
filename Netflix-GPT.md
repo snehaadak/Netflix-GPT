8292f26a0e7d3d128863e4b8c9826f3efc4d6244
Sneha <snehaadak321@gmail.com>
2025-08-15 19:37:07 +0530

    Initializing and creating Project structure

---
+++ Netflix-GPT.md
@@ -0,0 +1,15 @@
+## Netflix-GPT
+
+
+## Features:
+
+
+
+
+
+
+
+## Steps Summarized
+
+- Do `create-react-app` that creates a whole schefold of our project
+- Install and setup `tailwindcss`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl font-bold underline">Netflix GPT Project</p>
+    </div>
+  );
+}
+
+export default App;
